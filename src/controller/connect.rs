9ee106677e11101d43cc5474df99cebe2d8fea64
Briana Oursler <briana.oursler@gmail.com>
2021-05-29 16:00:58 -0700

    Controller: start_controller
    Separate start_controller and stream functionality so start can initiate
    other functions while stream receives from server.
    
    Signed-off-by: Briana Oursler <briana.oursler@gmail.com>

--- src/controller/connect.rs
+++ src/controller/connect.rs
@@ -1,7 +1,14 @@
+use crate::channel::Channel;
 use futures::prelude::*;
 use irc::client::prelude::*;
-#[tokio::main]
-pub async fn con(nick: &str, srv: &str, port: u16, use_tls: bool, channels: &[&str]) -> () {
+use std::sync::{Arc, Mutex};
+pub async fn start_client(
+    nick: &str,
+    srv: &str,
+    port: u16,
+    use_tls: bool,
+    channels: &[&str],
+) -> Client {
     let s = |s: &str| Some(s.to_owned());
     let config = Config {
         nickname: s(nick),
@@ -12,9 +19,16 @@ pub async fn con(nick: &str, srv: &str, port: u16, use_tls: bool, channels: &[&s
         ..Config::default()
     };
     let mut client = Client::from_config(config).await.unwrap();
+    run_stream(&mut client);
+    client
+}
+
+#[tokio::main]
+pub async fn run_stream(client: &mut Client) -> () {
     let mut stream = client.stream().unwrap();
     client.identify().unwrap();
     while let Some(m) = stream.next().await.transpose().unwrap() {
+        //rcv messages from server and send them to tui to print to screen
         println!("{:?}", m);
     }
 }
