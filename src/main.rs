2883c1ecc28075fbaf79812857b29d9d2dad797c
Lily Tsou <tsou@pdx.edu>
2021-06-06 17:59:00 -0700

    calling create_client() from main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod view;
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use async_std::task;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Event {
@@ -17,11 +18,13 @@ pub enum Event {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (con_send, con_rcv) = mpsc::channel();
-    let (mut tui, messages) = view::start_client(con_send.clone());
-    let port = 6667;
-    let use_tls = false;
+    //let (mut tui, messages) = view::start_client(con_send.clone());
+    
+    //let port = 6667;
+    //let use_tls = false;
     let ctlr = Arc::new(Mutex::new(None));
-
+    
+    /*
     let main_thread = thread::spawn(move || {
         loop {
             let message = con_rcv.recv().expect("receive channel closed");
@@ -29,8 +32,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Event::TuiMessage(name, message) => {
                     eprintln!("received message from {}: {}", name, message);
                     messages.append(format!("{}: {}\n", name, message));
-                    let ctlr = Arc::clone(&ctlr);
-                    controller::send(ctlr, &message);
+                    //let ctlr = Arc::clone(&ctlr);
+                    //controller::send(ctlr, &message);
                 }
                 Event::TuiQuit => {
                     eprintln!("quit");
@@ -45,7 +48,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         name,
                         channel,
                         server,
-                    ));*/
+                    ));
+                    */
                     let ctlr = Arc::clone(&ctlr);
                     let event_channel = con_send.clone();
                     let _ = thread::spawn(move || {
@@ -59,15 +63,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         let mut rcvr = ctlr.lock().unwrap();
                         *rcvr = Some(client);
                         drop(rcvr);
+                        //let client = client.identify();
+                        //messages.append(format!("{:?}", &client));
                         controller::start_receive(ctlr, event_channel)
                     });
                 }
-                _ => todo!()
+                _ => {
+                    break;
+                }
             }
         }
     });
 
+
+
+
     tui.run();
     main_thread.join().unwrap();
+    Ok(()) 
+}
+*/
+    println!("before thread");
+    let ctlr = Arc::clone(&ctlr);
+    let event_channel = con_send.clone();
+    let my_thread = thread::spawn(move || {
+        println!("inside thread");
+        let client = controller::create_client(
+           "lily",
+           "localhost.me",
+            6667,
+            false,
+            "#channel",
+        );
+        let mut rcvr = ctlr.lock().unwrap();
+        *rcvr = Some(client);
+        drop(rcvr);
+        controller::start_receive(ctlr, event_channel)
+    });
+
+    my_thread.join();
     Ok(())
 }
